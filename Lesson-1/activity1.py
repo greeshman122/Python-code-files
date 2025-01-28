93a6191113f0780c7f88c48aac8e83a683c04207
Greeshman <bunnymadgamer@gmail.com>
2025-01-28 19:41:22 +0530

    intial commit

---
+++ Lesson-1/activity1.py
@@ -0,0 +1,11 @@
+#print statements
+print ('Hello welcome to my first coding class')
+print ('Greeshman')
+print ('My age is ', 12)
+print ('Hurry', end='!!!')
+print ('\n Greeshman \n Trishul \n Geethak \n Koushik \n Simon')
+
+#function
+def helloworld():
+    print ("Hello")
+helloworld()
