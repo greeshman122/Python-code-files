0b47251e514fe20cbfa2045ec86deb9667f47951
Greeshman <bunnymadgamer@gmail.com>
2025-01-31 19:53:53 +0530

    intial commit

---
+++ lesson2/main.py
@@ -0,0 +1,17 @@
+Name='Greeshman'
+Age=14
+is_student=True
+Weight=55.6
+print("Name : ", type(Name))
+print("Age : ", type(Age))
+print("is_student : ", type(is_student))
+print("Weight : ", type(Weight))
+print("typecasting")
+a="6"
+print("before typecasting " , type(a))
+b=int(a)
+print("after typecasting " , type(b))
+c=34.5
+print("before typecasting " , type(c))
+d=int(c)
+print("after typecasting " , type(d))
