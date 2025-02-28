9db189b111203cf6ec8c2abd2256fbe539a6e09b
Greeshman <bunnymadgamer@gmail.com>
2025-02-28 19:47:28 +0530

    intial commit

---
+++ lesson7/main.py
@@ -0,0 +1,34 @@
+l1 = ["Mango" , "Strawberry" , "Apple" , "Watermelon" , "Papaya"]
+print(l1)
+
+#indexing
+print(l1[0])
+
+#Slicing
+print(l1[0:2])
+
+#Lenght of the list
+print(len(l1))
+
+#appending
+l1.append("Star Fruit")
+print(l1)
+
+#pop
+l1.pop()
+print(l1)
+
+l2 = [95 , 1000023 , 100000000003 , 1894784 , 379478975984]
+print(l2)
+
+#sorting
+l2.sort()
+print(l2)
+
+#reverse
+l2.reverse()
+print(l2)
+
+#clear
+l2.clear()
+print(l2)
