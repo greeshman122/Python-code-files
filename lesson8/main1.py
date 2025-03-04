df10aa72f71cea7649a1c09b1b84b775a4e0ef51
Greeshman <bunnymadgamer@gmail.com>
2025-03-04 20:06:39 +0530

    intial commit

---
+++ lesson8/main1.py
@@ -0,0 +1,18 @@
+s1 = { 1,2,3,4,5}
+s2 = { 4,5,6,7,8}
+
+#Intersection
+i = s1.intersection(s2)
+print(i)
+
+#Union
+u = s1.union(s2)
+print(u)
+
+#Difference
+d = s1.difference(s2)
+print(d)
+
+#Symmetric Difference
+sd = s1.symmetric_difference(s2)
+print(sd)
---
+++ lesson8/main4.py
@@ -0,0 +1,8 @@
+# Define a tuple
+my_tuple = (1, 2, 3, 4, 5)
+
+# Convert the tuple into a list
+my_list = list(my_tuple)
+
+# Print the list
+print(my_list)
