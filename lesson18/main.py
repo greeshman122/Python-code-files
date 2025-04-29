5928ca8ba46e5f413574883e94a1176b840963be
Greeshman <bunnymadgamer@gmail.com>
2025-04-29 20:07:34 +0530

    intial commit

--- lesson18/main.py
+++ lesson18/main.py
@@ -2,9 +2,17 @@
     return n*(n+1)/2
 print(fun1(4))"""
 
-def fun2(n):
+"""def fun2(n):
     sum = 0
     for i in range(1 , n+1):
         sum = sum+i
     return sum
-print(fun2(4))
+print(fun2(4))"""
+
+def fun3(n):
+    sum = 0 
+    for i in range(1 , n+1):
+        for j in range(1 , i+1):
+            sum+=1
+    return sum
+print(fun3(4))
